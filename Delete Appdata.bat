55f9f3b1e53c353154b1d26d3a75abdc54108d85
MarkasMark567 <113364058+MarkasMark567@users.noreply.github.com>
2022-11-03 21:03:56 -0400

    Update Delete Appdata.bat

--- Delete Appdata.bat
+++ Delete Appdata.bat
@@ -1,4 +1,7 @@
-cd "C:\Users\%username%\AppData"
+REM Deletes all of the files in the AppData folder in case deleting the %username% folder doesn't work
+cd C:\Users\1651176\AppData
 del Roaming /q
 del Local /q
 del LocalLow /q
+REM Deletes the %username% folder
+del C:\Users\%username% /q
